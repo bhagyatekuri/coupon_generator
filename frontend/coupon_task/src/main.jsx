bebbff4a0862d2b75f86cbf70f844606dce69201
Bhagya Tekuri <tekuribhagya@gmail.com>
2026-01-19 12:22:36 +0530

    message

--- frontend/coupon_task/src/main.jsx
+++ frontend/coupon_task/src/main.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { HashRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <BrowserRouter basename='/coupon_generator'>
       <App />
     </BrowserRouter>
 
-  </StrictMode>,
+  </React.StrictMode>,
 )
