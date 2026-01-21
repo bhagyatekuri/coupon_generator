bc72f586f4b8c2d22f93704a73fa0df04bd3dfb0
Bhagya Tekuri <tekuribhagya@gmail.com>
2026-01-21 19:03:00 +0530

    fix: remove base path for Render

--- frontend/coupon_task/vite.config.js
+++ frontend/coupon_task/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/coupon_generator/"
+  //base: "/coupon_generator/"
   // server: {
   //   host: true
   // }
